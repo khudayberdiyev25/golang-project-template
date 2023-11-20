a3b9d27cde9018f2666a7df7d7c495b28573a279
xbakhromjon <xbakhromjon@gmail.com>
2023-11-20 16:48:33 +0500

    .env file used in database connection string

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	_ "github.com/lib/pq"
-	"golang-project-template/internal/db"
+	"golang-project-template/internal/bootstrap"
 	"golang-project-template/internal/deliver/rest/api/router"
+	"log"
 	"net/http"
 )
 
 func main() {
-	db := db.Setup()
+	bootstrap.LoadEnv()
+	db := bootstrap.SetupDB()
 	r := chi.NewRouter()
 	router.Setup(r, db)
-	http.ListenAndServe(":5005", r)
+	err := http.ListenAndServe(":5005", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
